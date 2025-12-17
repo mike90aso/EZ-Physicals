00801ad83b40b21babf676aeabaaa266ea2da557
mike90aso <mike90.aso@gmail.com>
2025-12-17 18:02:15 -0500

    Add files via upload

---
+++ ez-physicals/locations.js
@@ -0,0 +1,294 @@
+// EZ Physicals Provider Locations
+const locations = [
+  {
+    name: "Action Chiropractic",
+    address: "5400 E Mockingbird Ln #214",
+    city: "Dallas",
+    state: "TX",
+    zip: "75206",
+    lat: 32.8407,
+    lng: -96.7698,
+    link: "#"
+  },
+  {
+    name: "Allegiant Health & Associates",
+    address: "130 Bates Ave SW, Unit 111",
+    city: "Winter Haven",
+    state: "FL",
+    zip: "33880",
+    lat: 28.0222,
+    lng: -81.7329,
+    link: "#"
+  },
+  {
+    name: "Allied Health & Rehabilitation",
+    address: "177 Salem Ct",
+    city: "Tallahassee",
+    state: "FL",
+    zip: "32301",
+    lat: 30.4383,
+    lng: -84.2807,
+    link: "#"
+  },
+  {
+    name: "Ascend Chiropractic",
+    address: "5611 NW 1st St Suite 105",
+    city: "Lincoln",
+    state: "NE",
+    zip: "68521",
+    lat: 40.8507,
+    lng: -96.7196,
+    link: "#"
+  },
+  {
+    name: "Back on Track Chiropractic",
+    address: "6888 Goodman Rd Suite 113",
+    city: "Olive Branch",
+    state: "MS",
+    zip: "38654",
+    lat: 34.9618,
+    lng: -89.8295,
+    link: "#"
+  },
+  {
+    name: "Better Life Chiropractic",
+    address: "235 Wadsworth Dr",
+    city: "Richmond",
+    state: "VA",
+    zip: "23236",
+    lat: 37.5007,
+    lng: -77.5636,
+    link: "#"
+  },
+  {
+    name: "Care Clinic Plus",
+    address: "136 E Colonial Dr",
+    city: "Orlando",
+    state: "FL",
+    zip: "32801",
+    lat: 28.5540,
+    lng: -81.3733,
+    link: "#"
+  },
+  {
+    name: "CarePlus Chiropractic Health Center",
+    address: "212 9th St, Ste 103",
+    city: "Oakland",
+    state: "CA",
+    zip: "94607",
+    lat: 37.7995,
+    lng: -122.2711,
+    link: "#"
+  },
+  {
+    name: "Chicago Injury Specialist, LLC",
+    address: "2548 S Blue Island Ave",
+    city: "Chicago",
+    state: "IL",
+    zip: "60608",
+    lat: 41.8456,
+    lng: -87.6617,
+    link: "#"
+  },
+  {
+    name: "Chiropractic Clinics of South Florida - Miami",
+    address: "701 NW 57th Ave #201",
+    city: "Miami",
+    state: "FL",
+    zip: "33126",
+    lat: 25.7751,
+    lng: -80.2729,
+    link: "#"
+  },
+  {
+    name: "Chiropractic Clinics of South Florida - North Miami",
+    address: "12550 Biscayne Blvd STE 404",
+    city: "North Miami",
+    state: "FL",
+    zip: "33181",
+    lat: 25.8926,
+    lng: -80.1628,
+    link: "#"
+  },
+  {
+    name: "Chiropractic Clinics of South Florida - West Palm Beach",
+    address: "2247 Palm Beach Lakes Blvd Ste 101",
+    city: "West Palm Beach",
+    state: "FL",
+    zip: "33409",
+    lat: 26.7153,
+    lng: -80.0905,
+    link: "#"
+  },
+  {
+    name: "Clifton D. Okman, DC",
+    address: "7244 W Oakland Park Blvd",
+    city: "Lauderhill",
+    state: "FL",
+    zip: "33313",
+    lat: 26.1684,
+    lng: -80.2261,
+    link: "#"
+  },
+  {
+    name: "Complete Chiropractic of Covington",
+    address: "2513 Chelsea Dr",
+    city: "Fort Mitchell",
+    state: "KY",
+    zip: "41017",
+    lat: 39.0284,
+    lng: -84.5594,
+    link: "#"
+  },
+  {
+    name: "Dr. Gene Lott",
+    address: "1007 W. Mitchell Street Ste. 102",
+    city: "Arlington",
+    state: "TX",
+    zip: "76013",
+    lat: 32.7357,
+    lng: -97.1211,
+    link: "#"
+  },
+  {
+    name: "George Samuel Jr, DC",
+    address: "3401 NW 9th Ave #200",
+    city: "Oakland Park",
+    state: "FL",
+    zip: "33309",
+    lat: 26.1684,
+    lng: -80.1528,
+    link: "#"
+  },
+  {
+    name: "Gulf Coast Health and Safety",
+    address: "8039 E Dr Martin Luther King Jr Blvd",
+    city: "Tampa",
+    state: "FL",
+    zip: "33619",
+    lat: 27.9659,
+    lng: -82.3862,
+    link: "#"
+  },
+  {
+    name: "Gutierrez Chiropractic Inc",
+    address: "1341 W Robinhood Dr #A7",
+    city: "Stockton",
+    state: "CA",
+    zip: "95207",
+    lat: 38.0049,
+    lng: -121.3382,
+    link: "#"
+  },
+  {
+    name: "Ideal Health Clinic LLC",
+    address: "3881 Eagle Creek Pkwy A",
+    city: "Indianapolis",
+    state: "IN",
+    zip: "46254",
+    lat: 39.8494,
+    lng: -86.2520,
+    link: "#"
+  },
+  {
+    name: "Maddalo Chiropractic & DOT Certification",
+    address: "14 McGrath Hwy, Twin City Plaza",
+    city: "Somerville",
+    state: "MA",
+    zip: "02143",
+    lat: 42.3803,
+    lng: -71.0892,
+    link: "#"
+  },
+  {
+    name: "MaRIA Adult Naturopathic Care",
+    address: "12719 SE 95th Pl",
+    city: "Renton",
+    state: "WA",
+    zip: "98056",
+    lat: 47.4799,
+    lng: -122.1826,
+    link: "#"
+  },
+  {
+    name: "Opachich Wellness Center",
+    address: "1610 Blanding Blvd",
+    city: "Jacksonville",
+    state: "FL",
+    zip: "32210",
+    lat: 30.2774,
+    lng: -81.7295,
+    link: "#"
+  },
+  {
+    name: "Pine Castle Chiropractic Center",
+    address: "707 E Oak Ridge Rd",
+    city: "Orlando",
+    state: "FL",
+    zip: "32809",
+    lat: 28.4697,
+    lng: -81.3678,
+    link: "#"
+  },
+  {
+    name: "DOT Physical Exam/Medical Card",
+    address: "2141 NW Military Hwy",
+    city: "San Antonio",
+    state: "TX",
+    zip: "78213",
+    lat: 29.5018,
+    lng: -98.5254,
+    link: "#"
+  },
+  {
+    name: "San Diego Chiropractic Wellness",
+    address: "2615 Camino del Rio S #100",
+    city: "San Diego",
+    state: "CA",
+    zip: "92108",
+    lat: 32.7647,
+    lng: -117.1373,
+    link: "#"
+  },
+  {
+    name: "Spectrum Health & Wellness",
+    address: "400 NW Eastman Pkwy",
+    city: "Gresham",
+    state: "OR",
+    zip: "97030",
+    lat: 45.5051,
+    lng: -122.4360,
+    link: "#"
+  },
+  {
+    name: "Titan Medical Center",
+    address: "1415 W 31st St. S",
+    city: "Wichita",
+    state: "KS",
+    zip: "67217",
+    lat: 37.6528,
+    lng: -97.3508,
+    link: "#"
+  },
+  {
+    name: "Tomoka Spine and Posture",
+    address: "595 W Granada Blvd Unit G",
+    city: "Ormond Beach",
+    state: "FL",
+    zip: "32174",
+    lat: 29.2858,
+    lng: -81.0909,
+    link: "#"
+  },
+  {
+    name: "Skyline Health Group",
+    address: "3939 Atlantic Ave Suite #114",
+    city: "Long Beach",
+    state: "CA",
+    zip: "90807",
+    lat: 33.8258,
+    lng: -118.1570,
+    link: "#"
+  }
+];
+
